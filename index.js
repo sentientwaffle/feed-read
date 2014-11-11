bcefeb5514b93d15260fea3504f2d2bdc41febad
Sung Wook Moon <mtjddnr@gmail.com>
2014-11-10 20:06:21 -0600

    add category

--- index.js
+++ index.js
@@ -181,6 +181,7 @@ FeedRead.rss = function(xml, source, callback) {
           , author:    child_data(art, "author")
                     || child_data(art, "dc:creator")
           , link:      child_data(art, "link")
+          , category:  child_data(art, "category")
           , feed:      meta
           };
         if (obj.published) obj.published = new Date(obj.published);
